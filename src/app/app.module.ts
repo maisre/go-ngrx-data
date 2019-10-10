aa36865e9e9ca7810a9175b7f5768a16b0d2aaa9
McKay Costley <McKay.Costley@asirobots.com>
2019-10-10 10:58:44 -0600

    added post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppStoreModule } from './store/app-store.module';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppStoreModule
+    AppStoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -3,12 +3,15 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
+import { DefaultDataServiceConfig, NgrxDataModule } from 'ngrx-data';
+import { entityConfig } from './entity-metadata';
 
 @NgModule({
   declarations: [],
   imports: [
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+    NgrxDataModule.forRoot(entityConfig),
     environment.production ? [] : StoreDevtoolsModule.instrument()
   ]
 })
