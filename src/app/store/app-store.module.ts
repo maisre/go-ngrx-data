7fa48afaac4c4b1ba768335bd2b9213d497ffc16
McKay Costley <McKay.Costley@asirobots.com>
2019-10-09 21:40:11 -0600

    added ngrx modules

---
+++ src/app/store/app-store.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    environment.production ? [] : StoreDevtoolsModule.instrument()
+  ]
+})
+export class AppStoreModule { }
